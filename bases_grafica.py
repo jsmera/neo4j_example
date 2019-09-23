481a1a23008e83e8b1c274c77b1bc70c3625b444
David <davidrivero731@gmail.com>
2019-09-23 16:24:58 -0500

    Grafica

---
+++ bases_grafica.py
@@ -0,0 +1,31 @@
+from sys import stdin
+
+def menu_Registrarse():
+    print("Registrese a continuacion")
+    print("--------------------------")
+    print("Ingrese")
+
+
+    
+
+def menu_Principal():
+    print("Bienvenido a la Base De Datos")
+    print("==============================")
+    print()
+    print("Seleccione una opcion")
+    print("1. Registrarte")
+    print("2. Sistema de preguntas")
+    print()
+    
+
+def main():
+    menu_Principal()
+    num=int(stdin.readline().strip())
+    while(num!=0):
+        if(num==1):
+            
+        num=int(stdin.readline().strip())
+
+
+    
+main()
